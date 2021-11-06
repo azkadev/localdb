27a666eeb4926d2d366781cd6efc6db7b2da2896
Azkadev <azkadev@pop-os.localdomain>
2021-11-06 21:24:15 +0700

    update

---
+++ Makefile
@@ -0,0 +1,4 @@
+run:
+	git add .
+	git commit -m update
+	git push origin
--- cpp/test.cpp
+++ cpp/test.cpp
@@ -1,28 +1,46 @@
 #include <iostream>
 #include <iomanip>
 #include </home/azkadev/Music/test/json.hpp>
+#include <fstream>
+#include <typeinfo>
+#include<bits/stdc++.h>
 
 using json = nlohmann::json;
 using namespace std;
 
-
 class JSON
 {
 public:
-    template<class TYPE>
+    template <class TYPE>
     void stringify(TYPE value, int space = 2)
     {
+        cout << setw(space) << value << '\n';
+    }
+    template <class TYPE>
+    void strinify(TYPE value, int space = 2)
+    {
+        return setw(space) << value << '\n';
+    }
+};
 
-        std::cout << std::setw(space) << value << '\n';
+class CONSOLE
+{
+public:
+    template <class TYPE>
+    void log(TYPE value = "")
+    {
+        cout << value << "\n";
     }
 };
 
 int main()
 {
     JSON jsonjs;
+    CONSOLE console;
     // create a JSON object
-    json j =
-        {};
+    json j = {};
+
+    ofstream MyFile("filename.json");
 
     // add new values
     j["new"]["key"]["value"] = {"another", "list"};
@@ -36,7 +54,26 @@ int main()
     j["size"] = s;
 
     // pretty print with indent of 4 spaces
-    jsonjs.stringify(j);
+    jsonjs.stringify("azkakzkaza");
+
+    int n = 36;
+    char c = 'A';
+    double d = 1.2;
+    if(*(typeid(n).name()) == 'i'){
+        cout << "I am an Integer variable" << endl;
+    }
+    if(*((char *) typeid(d).name()) == 'd'){
+        cout << "I am a Double variable" << endl;
+    }
+    if(*((char *) typeid(c).name()) == 'c'){
+        cout << "I am a Char variable" << endl;
+    }
+
+    // Write to the file
+    MyFile << setw(2) << j << '\n';
+
+    // Close the file
+    MyFile.close();
 }
 
 /*
